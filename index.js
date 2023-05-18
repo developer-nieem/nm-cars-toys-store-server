c3749ebd1e0502a1b97b104c277266d9f1890076
developer-nieem <naimhossenwork@gmail.com>
2023-05-18 23:56:46 +0600

    added delete option

--- index.js
+++ index.js
@@ -103,6 +103,13 @@ async function run() {
       res.send(result);
     });
 
+
+    app.delete('/deletetoy/:id' , async(req,res) =>{
+        const id =  req.params.id;
+        const filter =  {_id : new ObjectId(id)};
+        const result =  await carToysCollection.deleteOne(filter);
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
