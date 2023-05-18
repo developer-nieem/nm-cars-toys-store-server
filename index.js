de2b403e557962f2d0e18c55e49f528df8f5ddb8
developer-nieem <naimhossenwork@gmail.com>
2023-05-18 08:21:13 +0600

    connect mongoDB

--- index.js
+++ index.js
@@ -9,12 +9,8 @@ app.use(cors())
 app.use(express.json())
 
 
-console.log(process.env.DB_USER);
-
-
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.xifd9dy.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xifd9dy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,12 +25,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const carToysCollection = client.db('carToysDB').collection('carToys');
+    
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
